3f5b68f49c42f22c4e12e13101e06ad059cd4735
Douty <doutymanigat@gmail.com>
2023-11-30 15:57:51 -0500

    ui changes

--- client/src/components/BillSummaryCard.jsx
+++ client/src/components/BillSummaryCard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BillSummaryCard = () => {
+const BillSummaryCard = ({firstName, lastName, dueDate, paid,amountPaid}) => {
+  const [toggle,setToggle] = useState(false);
   return (
-    <div>BillSummaryCard</div>
+    <div>
+        <div onClick={()=>{toggle ? setToggle(false) : setToggle(true);}} className={`flex ${paid ? "bg-pastelGreen" : "bg-pastelRed" } gap-3 w-[350px] p-2 justify-between`}>
+
+          <div className='flex-col'>
+            <h1 className='font-bold'>{firstName}, {lastName}</h1>
+            <h1 className='text-sm'>{dueDate}</h1>
+          </div>
+
+          <div className='text-right'>
+            <h1>{amountPaid}</h1>
+            <h1 className='text-sm '>{dueDate}</h1>
+          </div>
+
+        </div>
+
+        <div className={`${toggle ? "show" : "hidden"} ${paid ? "bg-pastelGreen" : "bg-pastelRed" } p-2`}>
+          <h1>hi</h1>
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/EmployeePage.jsx
+++ client/src/pages/EmployeePage.jsx
@@ -4,17 +4,35 @@ import NavBar from '../components/NavBar'
 
 const EmployeePage = () => {
     const [query, setQuery] = useState("");
+  
 
   return (
     <div>
         <NavBar />
 
-        <div className='flex justify-center rounded-xl'>
+        <div className='flex flex-col items-center rounded-xl'>
             <input 
-                placeholder='Search'
+              placeholder='Search'
             />
-
         </div>
+        
+
+        <div className='flex flex-col items-center gap-2 py-4'>
+          <BillSummaryCard 
+            firstName="John" 
+            lastName="Doe" 
+            dueDate="December 24, 2023"
+            amountPaid = "$246"
+          />
+          <BillSummaryCard 
+            firstName="Alex" 
+            lastName="Dawn" 
+            dueDate="March, 23,2023" 
+            amountPaid = "$546"
+            paid={true} 
+          />
+        </div>
+        
 
         
     </div>
